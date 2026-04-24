25f48131a0309da41a5abf149b24530cd25296c4
Wilkerylr <lopezrewilker12@gmail.com>
2026-04-24 18:30:54 -0400

    configuraciones para despliegue en netlify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Nombre del repositorio en GitHub para GitHub Pages
-  base: '/portafolio-Wilkerylr/',
+  // Base path para Netlify (raíz relativa)
+  base: process.env.CONTEXT === 'production' ? '/' : '/portafolio-Wilkerylr/',
   plugins: [react()],
   server: {
     proxy: {
